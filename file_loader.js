6c03ebe090cb61307406e999d94c544ad14e8c73
Croo <croohmanoor@gmail.com>
2013-10-28 20:37:26 +0100

    Refactored file_loader.js.

--- file_loader.js
+++ file_loader.js
@@ -1,21 +1,10 @@
-var red_wine;
-var white_wine;
 
 $(document).ready(function(){
-
-    $.get("wine/wine_red.csv",
-            function(data){
-                red_wine = $.csv.toObjects(data);
-                drawScatterplot(red_wine);
-            },
-        "text"
-    );
-
-    $.get("wine/wine_white.csv",
-            function(data){
-                white_wine = $.csv.toObjects(data);
-                drawScatterplot(white_wine);
-            },
-        "text"
-    );
+    $.get("wine/wine_red.csv", success, "text");
+    $.get("wine/wine_white.csv", success, "text");
 });
+
+function success(data) {
+    dataset = $.csv.toObjects(data);
+    drawScatterplot(dataset);
+}
