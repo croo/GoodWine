15b8b0d6851e4ecfc5f141cc998e402c1f1fbd31
Croo <croohmanoor@gmail.com>
2013-10-29 21:57:29 +0100

    refaktoráltam nem túl sok sikerrel, végül kijavítottam azt a hibát, hogy plot váltás után a checkboxok nem működtek / mind a két bort megint kirajzolták. Most már a scatterplot Pöpec.

--- drawer.js
+++ drawer.js
@@ -1,41 +1,41 @@
-var white_wines = {"loaded":false};
-var red_wines = {"loaded":false};
-var wines;
+var wines = {};
+wines.white = {};
+wines.red = {};
 
-var columns = ["fixed_acidity","volatile_acidity","citric_acid","residual_sugar","chlorides","free_sulfur_dioxide","total_sulfur_dioxide","density","pH","sulphates","alcohol","quality"];
+wines.columns = ["fixed_acidity","volatile_acidity","citric_acid","residual_sugar","chlorides","free_sulfur_dioxide","total_sulfur_dioxide","density","pH","sulphates","alcohol","quality"];
 
 $(document).ready(function(){
 
    var intervalId = setInterval(function(){
-                                    if(white_wines.loaded && red_wines.loaded){
+                                    if(wines.white.loaded && wines.red.loaded){
                                         clearInterval(intervalId);
                                         initDraw();
                                     }
                                 },
                                 200);
 
-
     $.get("wine/wine_red.csv", red_success, "text");
     $.get("wine/wine_white.csv", white_success, "text");
 });
 
 function red_success(data) {
-    red_wines = $.csv.toObjects(data);
-    red_wines.loaded = true;
-    for(var i=0; i < red_wines.length;i++) {
-        red_wines[i].wine_type = "red";
+    wines.red = $.csv.toObjects(data);
+    wines.red.loaded = true;
+    wines.red.visible = true;
+    for(var i = 0; i < wines.red.length; i++) {
+        wines.red[i].type = "red";
     }
 }
 
 function white_success(data) {
-    white_wines = $.csv.toObjects(data);
-    white_wines.loaded = true;
-    for(var i=0; i < white_wines.length;i++) {
-        white_wines[i].wine_type = "white";
+    wines.white = $.csv.toObjects(data);
+    wines.white.loaded = true;
+    wines.white.visible = true;
+    for(var i = 0; i < wines.white.length; i++) {
+        wines.white[i].type = "white";
     }
 }
 
 function initDraw() {
-    wines = red_wines.concat(white_wines);
-    drawScatterplot(wines,columns[0],columns[6]);
+    drawScatterplot(wines.red.concat(wines.white),wines.columns[0],wines.columns[6]);
 }
--- scatterplot.js
+++ scatterplot.js
@@ -43,7 +43,7 @@ function drawScatterplot(dataset,xValue,yValue) {
                             return yScale(d[yValue]);
                             })
                     .attr("r", function (d) { return Math.pow(d["quality"]/2,1.8);}) // exponential sizing for great good
-                    .attr("class",function(d){return d.wine_type=="red"?"red_wine":"white_wine";});
+                    .attr("class",function(d){return d.type=="red"?"red_wine":"white_wine";});
 
     var xAxis = d3.svg.axis().scale(xScale).orient("top");
     var yAxis = d3.svg.axis().scale(yScale).orient("right");
@@ -58,26 +58,43 @@ function drawScatterplot(dataset,xValue,yValue) {
 
 // wire events
 $(document).ready(function() {
+
+    scatterplotX = wines.columns[0];
+    scatterplotY = wines.columns[6];
+
     $("#data_x").change(function(){
         scatterplotX = $("#data_x").val();
-        drawScatterplot(wines,scatterplotX,scatterplotY);
+        drawScatterplot(wines.red.concat(wines.white),scatterplotX,scatterplotY);
+        if(!wines.red.visible) {
+            $(".red_wine").hide();
+        }
+        if(!wines.white.visible) {
+            $(".white_wine").hide();
+        }
     });
 
     $("#data_y").change(function(){
         scatterplotY = $("#data_y").val();
-        drawScatterplot(wines,scatterplotX,scatterplotY);
+        var dataset = [];
+        drawScatterplot(wines.red.concat(wines.white),scatterplotX,scatterplotY);
+        if(!wines.red.visible) {
+            $(".red_wine").hide();
+        }
+        if(!wines.white.visible) {
+            $(".white_wine").hide();
+        }
     });
-    
+
+    // checkbox before scatterplot for red and white wine data
     $("#dataset_selectbox input").change(function() {
         $("#dataset_selectbox input").each(function(i,checkbox) {
             if(checkbox.checked) {
                 $("."+checkbox.name).show();
+                wines[checkbox.value].visible = true;
             } else {
                 $("." + checkbox.name).hide();
+                wines[checkbox.value].visible = false;
             }
         });
     });
-
-    scatterplotX = columns[0];
-    scatterplotY = columns[6];
 });
