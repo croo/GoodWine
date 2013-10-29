99d8b8b259c3ff8f62f6d173272672a65138abdb
Croo <croohmanoor@gmail.com>
2013-10-29 13:27:56 +0100

    Az util.js-ben lévő scale függvény fölösleges, mert a d3 meg tudja csinálni, nagyon elegánsan, 2 sorból. A test.html meg véletlenül került be...

--- util.js
+++
@@ -1,9 +0,0 @@
-function scaleToRange(o) {
-  var x = o.x;
-  var A = o.min;
-  var B = o.max;
-  var C = o.range_min;
-  var D = o.range_max;
-
- return C*(1 - ((x - A) /(B - A))) + D*((x - A)/(B - A));
-}
